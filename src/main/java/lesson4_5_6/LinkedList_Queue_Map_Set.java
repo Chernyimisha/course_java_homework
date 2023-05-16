e3d90c65fc79df90349c0e4b6cd8548c6f1b2c6a
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-05-16 20:38:14 +0300

    save as

--- src/main/java/lesson4_5_6/LinkedList_Queue_Map_Set.java
+++ src/main/java/lesson4_5_6/LinkedList_Queue_Map_Set.java
@@ -1,8 +1,6 @@
 package lesson4_5_6;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class LinkedList_Queue_Map_Set {
     public static void main(String[] args) {
@@ -65,5 +63,67 @@ public class LinkedList_Queue_Map_Set {
 
 //        Вычислить значение выражения в постфиксной форме записи
 //        1 + 2 * 3 (1 + 2) * 3
+
+        Map<Integer, String> db = new HashMap<>();
+        db.put(1, "один"); System.out.println(db);
+        db.put(2, "два"); System.out.println(db);
+        db.put(3, "три"); System.out.println(db);
+        db.put(31, "три один"); System.out.println(db);
+        db.put(13, "один три"); System.out.println(db);
+        db.put(null, "!null"); System.out.println(db);
+        db.put(null, null); System.out.println(db);
+
+        for (var item1 : db.entrySet()) {
+            System.out.printf("[%d: %s]\n", item1.getKey(), item1.getValue());
+        }
+
+
+        //HashMap
+
+//        put(K,V) – добавить пару если или изменить значение,если ключ имеется.
+//        putIfAbsent(K,V) – произвести добавление если ключ не найден.
+//        get(K) - получение значения по указанному ключу.
+//        remove(K) – удаляет пару по указанному ключу.
+//        containsValue(V) – проверка наличия значения.
+//        containsKey(V) – проверка наличия ключа.
+//        keySet() – возвращает множество ключей.
+//        values() – возвращает набор значений.
+
+//        Map<Integer,String> map1 = new HashMap<>();
+//        Map<Integer,String> map2 = new HashMap<>(9);
+//        Map<Integer,String> map3 = new HashMap<>(9, 1.0f);
+
+        TreeMap<Integer,String> tMap = new TreeMap<>();
+        tMap.put(1,"один"); System.out.println(tMap);
+        // {1=один}
+        tMap.put(6,"шесть"); System.out.println(tMap);
+        // {1=один, 6=шесть}
+        tMap.put(4,"четыре"); System.out.println(tMap);
+        // {1=один, 4=четыре, 6=шесть}
+        tMap.put(3,"три"); System.out.println(tMap);
+        // {1=один, 3=три, 4=четыре, 6=шесть}
+        tMap.put(2,"два"); System.out.println(tMap);
+        // {1=один, 2=два, 3=три, 4=четыре, 6=шесть}
+
+//        put(K,V) get(K) remove(K)
+//        descendingMap() tailMap() headMap() lastEntry() firstEntry()
+
+        Map<Integer,String> linkmap = new LinkedHashMap<>();
+        linkmap.put(11, "один один");
+        linkmap.put(1, "два");
+        linkmap.put(2, "один");
+        System.out.println(linkmap); // {11=один один, 1=два, 2=один}
+        Map<Integer,String> map = new HashMap<>();
+        map.put(11, "один один");
+        map.put(2, "два");
+        map.put(1, "один");
+        System.out.println(map); // {1=один, 2=два, 11=один один}
+
+        Map<Integer,String> table = new Hashtable<>();
+        table.put(1, "два");
+        table.put(11, "один один");
+        table.put(2, "один");
+        System.out.println(table); // {2=один, 1=два, 11=один один}
+        // table.put(null, "один"); // java.lang.NullPointerException
     }
 }
---
+++ src/main/java/lesson4_5_6/MainClassFromLessonFive.java
@@ -0,0 +1,7 @@
+package lesson4_5_6;
+
+public class MainClassFromLessonFive {
+    public static void main(String[] args) {
+
+    }
+}
