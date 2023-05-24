506661eda20b9b4a9edcc0c3cf6a08bd5acae80f
Proshkin Mikhail SIL <chernyimisha@rambler.ru>
2023-05-24 18:11:39 +0300

    extract part1, part2

--- src/main/java/lesson4_5_6/LinkedList_Queue_Map_Set.java
+++ src/main/java/lesson4_5_6/LinkedList_Queue_Map_Set.java
@@ -125,5 +125,86 @@ public class LinkedList_Queue_Map_Set {
         table.put(2, "один");
         System.out.println(table); // {2=один, 1=два, 11=один один}
         // table.put(null, "один"); // java.lang.NullPointerException
+
+//        Set
+//        HashSet
+//        isEmpty() – проверка на пустоту.
+//        add(V) – добавление элемента в коллекцию.
+//        remove(V) – удаление элемента из коллекцию.
+//        contains(V) – проверка на включение элемента в коллекции.
+//        clear() – удаление всех элементов коллекции.
+//        size() – возвращает количество элементов коллекции.
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1); set.add(12); set.add(123);
+        set.add(1234); set.add(1234);
+        System.out.println(set.contains(12)); // true
+        set.add(null);
+        System.out.println(set.size()); // 5
+        System.out.println(set); // [null, 1, 1234, 123, 12]
+        set.remove(12);
+        for (var item5 : set) System.out.println(item5); // null 1 1234 123
+        set.clear();
+        System.out.println(set); // []
+
+//        HashSet как синоним множества
+//        addAll(Coll) – объединение множеств.
+//        retainAll(Coll) – пересечение множеств.
+//        removeAll(Coll) – разность множеств.
+        var a = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
+        var b = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17));
+        var u = new HashSet<Integer>(a); u.addAll(b);
+        var r = new HashSet<Integer>(a); r.retainAll(b);
+        var s = new HashSet<Integer>(a); s.removeAll(b);
+        System.out.println(a); // [1, 2, 3, 4, 5, 6]
+        System.out.println(b); // [17, 2, 3, 5, 7, 11]
+        System.out.println(u); // [1, 17, 2, 3, 4, 5, 6, 11]
+        System.out.println(r); // [2, 3, 5, 7]
+        System.out.println(s); // [1, 4, 6]
+
+//        first()
+//        last()
+//        headSet(E)
+//        tailSet(E)
+//        subSet(E1, E2)
+        var a6 = new TreeSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b6 = new TreeSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        System.out.println(a6); // [1, 2, 3, 4, 5, 6, 7]
+        System.out.println(b6); // [2, 3, 5, 7, 11, 13, 17]
+//        System.out.println(a.headSet(4)); // [1, 2, 3]
+//        System.out.println(a.tailSet(4)); // [4, 5, 6, 7]
+//        System.out.println(a.subSet(3, 5)); // [3, 4]
+
+//        TreeSet
+
+//    ● В основе HashMap.
+//    ● Упорядочен по возрастанию.
+//    ● null’ов быть не может.
+
+        var a7 = new TreeSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b7= new TreeSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        System.out.println(a7); // [1, 2, 3, 4, 5, 6, 7]
+        System.out.println(b7); // [2, 3, 5, 7, 11, 13, 17]
+        System.out.println(a.contains(1)); // true
+
+//        LinkedHashSet
+
+//    ● В основе HashMap.
+//    ● Помнит порядок.
+//    Использовать, когда нужен HashSet с запоминанием порядка элемента.
+//        isEmpty() – проверка на пустоту.
+//        add(V) – добавление элемента в коллекцию.
+//        remove(V) – удаление элемента из коллекцию.
+//        contains(V) – проверка на включение элемента в коллекции.
+//        clear() – удаление всех элементов коллекции.
+//        size() – возвращает количество элементов коллекции.
+
+        var a8 = new LinkedHashSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b8 = new LinkedHashSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        a.add(28);
+        System.out.println(a8); // [1, 7, 2, 3, 6, 4, 5, 28]
+        System.out.println(b8); // [13, 3, 17, 7, 2, 11, 5]
+
+
     }
 }
